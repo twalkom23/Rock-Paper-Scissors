ddafe079ff7b90dc7139309404410c072d4993ab
Tim Walkom <timwalkom@hotmail.com>
2023-11-04 08:25:18 +1030

    Created the Play Round function

--- myscript.js
+++ myscript.js
@@ -12,8 +12,47 @@ function getComputerChoice() {
     }
 }
 
-let result = getComputerChoice();
-alert(result)
+let playerMove = "Rock";
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Rock" && computerSelection == "Rock") {
+        return "Draw! You both selected Rock!";
+    }
+    else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return "Computer Wins! Paper beats Rock!";
+    }
+    else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return "Player Wins! Rock beats Scissors!";
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        return "Player Wins! Paper beats Rock!";
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Paper") {
+        return "Draw! You both selected Paper!";
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        return "Computer Wins! Scissors beats Paper!";
+    }
+    else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        return "Computer Wins! Rock beats Scissors";
+    }
+    else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        return "Player Wins! Scissors beats Paper";
+    }
+    else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
+        return "Draw! You both selected Scissors!"
+    }
+    else {
+        return "Invalid Input";
+    }  
+}
+
+
+
+
+
+
 
 
 
