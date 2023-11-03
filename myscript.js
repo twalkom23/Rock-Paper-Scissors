43b11ea2870dbbd1a571cf9a7476f50e90917c7f
Tim Walkom <timwalkom@hotmail.com>
2023-11-04 08:34:57 +1030

    playGame function no longer case sens

--- myscript.js
+++ myscript.js
@@ -12,41 +12,47 @@ function getComputerChoice() {
     }
 }
 
-let playerMove = "Rock";
+let playerMove = "ROCK";
 
 
+//Creating the Play round function to compare the players moves and giving a winner.
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "Rock" && computerSelection == "Rock") {
+    //Changing the selection to lower case to improve the likelihood the game isnt invalid
+    let playerSelection1 = playerSelection.toLowerCase();
+    let computerSelection1 = computerSelection.toLowerCase();
+    //Comparing moves to declare a winner
+    if (playerSelection1 == "rock" && computerSelection1 == "rock") {
         return "Draw! You both selected Rock!";
     }
-    else if (playerSelection == "Rock" && computerSelection == "Paper") {
+    else if (playerSelection1 == "rock" && computerSelection1 == "paper") {
         return "Computer Wins! Paper beats Rock!";
     }
-    else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    else if (playerSelection1 == "rock" && computerSelection1 == "scissors") {
         return "Player Wins! Rock beats Scissors!";
     }
-    else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    else if (playerSelection1 == "paper" && computerSelection1 == "rock") {
         return "Player Wins! Paper beats Rock!";
     }
-    else if (playerSelection == "Paper" && computerSelection == "Paper") {
+    else if (playerSelection1 == "paper" && computerSelection1 == "paper") {
         return "Draw! You both selected Paper!";
     }
-    else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    else if (playerSelection1 == "paper" && computerSelection1 == "scissors") {
         return "Computer Wins! Scissors beats Paper!";
     }
-    else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    else if (playerSelection1 == "scissors" && computerSelection1 == "rock") {
         return "Computer Wins! Rock beats Scissors";
     }
-    else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    else if (playerSelection1 == "scissors" && computerSelection1 == "paper") {
         return "Player Wins! Scissors beats Paper";
     }
-    else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
+    else if (playerSelection1 == "scissors" && computerSelection1 == "scissors") {
         return "Draw! You both selected Scissors!"
     }
     else {
         return "Invalid Input";
     }  
 }
+
 
 
 
