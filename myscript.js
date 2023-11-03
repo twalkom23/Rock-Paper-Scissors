2e507d6ab1837b42fc85489e0d0c3551d77b9cdd
Tim Walkom <timwalkom@hotmail.com>
2023-11-03 19:08:43 +1030

    Created the Computer choice function

--- myscript.js
+++ myscript.js
@@ -1,1 +1,19 @@
+//creates the computers rock, paper, scissors choice at random
+function getComputerChoice() {
+    let compChoice = Math.floor(Math.random() * 3) + 1;
+    if (compChoice === 1) {
+        return "Rock";
+    }
+    else if (compChoice === 2) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+let result = getComputerChoice();
+alert(result)
+
+
 
